bf4acf1cdf8d6f0bb6392d1e259367776542b72f
max <harumaxy@gmail.com>
2021-01-22 13:40:02 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+pub mod nakama {
+    pub mod api {
+        include!("gen/nakama.api.rs");
+    }
+    pub mod realtime {
+        include!("gen/nakama.realtime.rs");
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
