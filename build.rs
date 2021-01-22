a247c444226704db364f84b2f2a3fa4a53f2ccda
max <harumaxy@gmail.com>
2021-01-22 13:33:57 +0900

    update build script

--- build.rs
+++ build.rs
@@ -1,18 +1,19 @@
 fn main() {
+    let import_paths = &[
+        "nakama/apigrpc",
+        "nakama/vendor",
+        "nakama/build/grpc-gateway-v2.0.1/third_party/googleapis",
+        "nakama/vendor/github.com/grpc-ecosystem/grpc-gateway/v2",
+    ];
+
+    let protos = &[
+        "nakama/apigrpc/apigrpc.proto",
+        "nakama/vendor/github.com/heroiclabs/nakama-common/api/api.proto",
+        "nakama/vendor/github.com/heroiclabs/nakama-common/rtapi/realtime.proto",
+    ];
     tonic_build::configure()
-        .compile(
-            &[
-                "nakama/apigrpc/apigrpc.proto",
-                "nakama/vendor/github.com/heroiclabs/nakama-common/api/api.proto",
-                "nakama/vendor/github.com/heroiclabs/nakama-common/rtapi/realtime.proto",
-            ],
-            &[
-                "nakama/apigrpc",
-                "nakama/vendor",
-                "nakama/build/grpc-gateway-v2.0.1/third_party/googleapis",
-                "nakama/vendor/github.com/grpc-ecosystem/grpc-gateway/v2",
-            ],
-        )
-        .map_err(|e| println!("{}", e))
+        .out_dir("src/gen")
+        .build_server(false)
+        .compile(protos, import_paths)
         .unwrap();
 }
