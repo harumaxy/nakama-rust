46b00d45d2cb934898466c9d17ac6db5ec562de5
max <harumaxy@gmail.com>
2021-01-25 04:00:14 +0900

    add connect_with_session_token & rename before one to connect_with_sever_key

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,11 @@ pub mod nakama;
 
 pub mod util {
 
+    use crate::nakama::{self, api::Session};
     use nakama::api::nakama_client::NakamaClient;
     use tonic::{metadata::MetadataValue, transport::*, Request};
 
-    use crate::nakama;
-
-    pub async fn connect_with_authrization(
+    pub async fn connect_with_server_key(
         address: Option<&'static str>,
         server_key: Option<&'static str>,
     ) -> Result<NakamaClient<Channel>, tonic::transport::Error> {
@@ -26,7 +25,28 @@ pub mod util {
                 .map(|map| println!("{:?}", map));
             Ok(req)
         });
+        Ok(client)
+    }
 
+    pub async fn connect_with_session_token(
+        address: Option<&'static str>,
+        session: &Session,
+    ) -> Result<NakamaClient<Channel>, tonic::transport::Error> {
+        let address = address.unwrap_or("https://[::1]:7349");
+        let token = session.token.clone();
+        let channel = tonic::transport::Channel::from_static(address)
+            .connect()
+            .await?;
+        let auth_string = format!("Bearer {}", token);
+        let client = NakamaClient::with_interceptor(channel, move |mut req: Request<()>| {
+            req.metadata_mut()
+                .insert(
+                    "authorization",
+                    MetadataValue::from_str(auth_string.as_str()).unwrap(),
+                )
+                .map(|map| println!("{:?}", map));
+            Ok(req)
+        });
         Ok(client)
     }
 }
--- tests/auth.rs
+++ tests/auth.rs
@@ -1,10 +1,12 @@
 use nakama_rust_gen::nakama::api::{
-    AccountDevice, AccountEmail, AuthenticateDeviceRequest, AuthenticateEmailRequest,
+    nakama_client::NakamaClient, AccountDevice, AccountEmail, AuthenticateDeviceRequest,
+    AuthenticateEmailRequest, Session,
 };
-use tonic::Request;
+use tonic::{transport::Channel, Request, Response, Status};
 
-#[tokio::test]
-async fn email_authenticate() {
+pub async fn email_authenticate(
+    client: &mut NakamaClient<Channel>,
+) -> Result<Response<Session>, Status> {
     let email_auth_request = Request::new(AuthenticateEmailRequest {
         create: Some(true),
         account: Some(AccountEmail {
@@ -15,10 +17,16 @@ async fn email_authenticate() {
         username: "max".into(),
     });
 
-    let mut client = nakama_rust_gen::util::connect_with_authrization(None, None)
+    let response = client.authenticate_email(email_auth_request).await;
+    response
+}
+
+#[tokio::test]
+async fn email_authenticate_test() {
+    let mut client = nakama_rust_gen::util::connect_with_server_key(None, None)
         .await
         .unwrap();
-    let response = client.authenticate_email(email_auth_request).await;
+    let response = email_authenticate(&mut client).await;
 
     assert!(response
         .map(|session| println!("Ok： {:?}", session))
@@ -39,7 +47,7 @@ async fn device_authenticate() {
         }),
     });
 
-    let mut client = nakama_rust_gen::util::connect_with_authrization(None, None)
+    let mut client = nakama_rust_gen::util::connect_with_server_key(None, None)
         .await
         .unwrap();
     let response = client.authenticate_device(device_auth_request).await;
