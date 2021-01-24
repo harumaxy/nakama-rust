5a1b2bcd33b6868862079cad953ddb43fe4d60c6
max <harumaxy@gmail.com>
2021-01-25 05:02:50 +0900

    update storage test

--- tests/storage.rs
+++ tests/storage.rs
@@ -2,7 +2,11 @@
 extern crate serde;
 extern crate serde_json;
 
-use nakama_rust_gen::nakama::api::{WriteStorageObject, WriteStorageObjectsRequest};
+use nakama_rust_gen::nakama::api::{
+    DeleteStorageObjectId, DeleteStorageObjectsRequest, ReadStorageObjectId,
+    ReadStorageObjectsRequest, WriteStorageObject, WriteStorageObjectsRequest,
+};
+use serde::__private::de;
 
 mod test_utils;
 
@@ -16,7 +20,7 @@ struct Weapon {
 async fn write_object() {
     let mut client = test_utils::make_session_client().await.unwrap();
     let sword = Weapon {
-        name: "Storm Sword".to_string(),
+        name: "Storm Sword⚔️".to_string(),
         atk: 999,
     };
     let value = serde_json::to_string(&sword).unwrap();
@@ -28,7 +32,7 @@ async fn write_object() {
             key: "weapon".into(),
             value: value.into(),
             permission_read: Some(2), // "Public Read" (2), "Owner Read" (1), or "No Read" (0).
-            permission_write: Some(0), // "Owner Write" (1), or "No Write" (0)
+            permission_write: Some(1), // "Owner Write" (1), or "No Write" (0)
             ..Default::default()
         }]
         .into(),
@@ -43,5 +47,51 @@ async fn write_object() {
 
 #[tokio::test]
 async fn read_object() {
-    assert!(true)
+    let mut client = test_utils::make_session_client().await.unwrap();
+    let self_account = client.get_account(()).await.unwrap().into_inner();
+    let req = ReadStorageObjectsRequest {
+        object_ids: [ReadStorageObjectId {
+            collection: "equipment".into(),
+            key: "weapon".into(),
+            user_id: self_account.user.unwrap().id,
+        }]
+        .into(),
+    };
+    let response = client.read_storage_objects(req).await;
+    assert!(response
+        .map(|storage_objs| println!("Ok： {:?}", storage_objs))
+        .map_err(|err| println!("Err： {}", err))
+        .is_ok())
+}
+
+#[tokio::test]
+async fn delete_object() {
+    let mut client = test_utils::make_session_client().await.unwrap();
+    let self_account = client.get_account(()).await.unwrap().into_inner();
+    let req = ReadStorageObjectsRequest {
+        object_ids: [ReadStorageObjectId {
+            collection: "equipment".into(),
+            key: "weapon".into(),
+            user_id: self_account.user.unwrap().id,
+        }]
+        .into(),
+    };
+    let read_response = client.read_storage_objects(req).await.unwrap().into_inner();
+    let delete_ids = read_response
+        .objects
+        .iter()
+        .map(|r| DeleteStorageObjectId {
+            collection: r.collection.clone(),
+            key: r.key.clone(),
+            version: r.version.clone(),
+        })
+        .collect();
+    let req = DeleteStorageObjectsRequest {
+        object_ids: delete_ids,
+    };
+    let response = client.delete_storage_objects(req).await;
+    assert!(response
+        .map(|none| println!("Ok： {:?}", none))
+        .map_err(|err| println!("Err： {}", err))
+        .is_ok())
 }
